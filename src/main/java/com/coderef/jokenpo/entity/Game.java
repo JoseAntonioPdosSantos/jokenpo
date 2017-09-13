532da3e733b0022165d59db11e46d39ea3b3a732
japsantos <japsantos@fazenda.ms.gov.br>
2017-09-13 17:17:08 -0400

    Implementando o resultado do jogo

--- src/main/java/com/coderef/jokenpo/entity/Game.java
+++ src/main/java/com/coderef/jokenpo/entity/Game.java
@@ -2,30 +2,53 @@ package com.coderef.jokenpo.entity;
 
 import com.coderef.jokenpo.service.Attack;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public abstract class Game {
-	
+
+    private Attack attack;
 	protected int size;
 	protected int weight;
 	protected int utility;
 	private int losses;
-	
+
+	public Game(){
+	    attack = new Attack();
+        init();
+    }
+
 	public Game attack(Game game){
-		Attack.toAttack(this, game);
-		
-		return getWinner(game);
+		attack.toAttack(this, game);
+		attack.getWinner(this, game);
+		return null;
 	}
 	
 	public void addLosses(){
 		losses++;
 	}
-	
-	private Game getWinner(Game game){
-		if(this.losses == game.losses)
-			return new Draw();
-		if(this.losses > game.losses)
-			return this;
-		else
-			return game;
+
+	public int getLosses(){
+		return losses;
 	}
-	
+
+	private void init(){
+        Properties properties = new Properties();
+        InputStream in = this.getClass().getResourceAsStream("resultGame.properties");
+        try {
+            properties.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+	public void getWinner(){
+
+    }
 }
--- src/main/java/com/coderef/jokenpo/service/Attack.java
+++ src/main/java/com/coderef/jokenpo/service/Attack.java
@@ -1,13 +1,30 @@
 package com.coderef.jokenpo.service;
 
+import com.coderef.jokenpo.entity.Draw;
 import com.coderef.jokenpo.entity.Game;
+import com.coderef.jokenpo.result.ResultGame;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Attack {
 
-	public static void toAttack(Game game1, Game game2){
+	public void toAttack(Game game1, Game game2){
 		new SizeAttack(game1, game2);
 		new WeightAttack(game1, game2);
 		new UtilityAttack(game1, game2);
+	}
+
+	public ResultGame getWinner(Game game1, Game game2){
+
+
+        if(game1.getLosses() == game2.getLosses())
+            return new DrawResult();
+        if(game1.getLosses() > game2.getLosses())
+            return game1;
+        else
+            return game;
 	}
 	
 }
